ce0de8de3f4be45a0afac392156f0953135147c1
mmlyujin <109034378+mmlyujin@users.noreply.github.com>
2022-08-11 15:07:43 +0900

    2022-08-11 node 연동

--- node_mysql_ver01/server.js
+++ node_mysql_ver01/server.js
@@ -12,6 +12,7 @@ const dbconnect = mysql.createConnection({
   user: "busanit",
   password: "1234",
   database: "busanit",
+  dateStrings: "date",
 });
 // 1. alter user 'busanit'@'%'identified with mysql_native_password by'1234';
 // 2. flush privileges;
@@ -52,10 +53,42 @@ app.get("/register", (req, res) => {
   res.render("register", { title: "Home > Register" }); //파일 이름 적어주면 됨. -확장자x
 });
 
+app.post("/register", (req, res) => {
+  /* console.log(req.body.userid); 터미널에 html 에 적은 userid값이 찍힌다.
+  console.log(req.body.passwd);
+  console.log(req.body.username);*/
+  //console.log(req.body); //값 전부 가져옴.
+  const sql = "INSERT INTO tb_users VALUES(null,?,?,?,now())";
+  //db접속 -> insert 실행하기 위한 코드
+  dbconnect.query(
+    sql,
+    [req.body.userid, req.body.passwd, req.body.username],
+    (err) => {
+      //입출력 : 예외처리..
+      if (!err) {
+        console.log("회원가입이 완료되었습니다.");
+        res.redirect("/list");
+        //mysql 에서 select * from tb_users; 하면 register에 입력한 값 테이블에 들어간 것 확인.
+      } else {
+        console.log(err);
+        console.log("관리자에 문의하세요.");
+        res.redirect("/register");
+      }
+    }
+  );
+
+  //const { userid, passwd, username } = req.body;
+});
+
 app.get("/list", (req, res) => {
   //console.log("콘솔에서 보이는 글자");
   //res.send("브라우저에서 글자가 보임");
-  res.render("list", { title: "Home > List" }); //파일 이름 적어주면 됨. -확장자x
+  //res.render("list", { title: "Home > List" }); //파일 이름 적어주면 됨. -확장자x
+  const sql = "SELECT * FROM tb_users ORDER BY num DESC";
+  dbconnect.query(sql, (err, results) => {
+    console.log(results);
+    res.render("list", { title: "Home > List", users: results });
+  });
 });
 
 /* 6. end --------------------------------------------------------------- */
