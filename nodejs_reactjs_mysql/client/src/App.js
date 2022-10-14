b49674b22bed37787b2fc714497c4c43edc68adf
mmlyujin <mmlyujin@gmail.com>
2022-10-14 11:22:38 +0900

    2022-10-14 table에 mysql table 데이터 연결해서 html에 띄움

--- nodejs_reactjs_mysql/client/src/App.js
+++ nodejs_reactjs_mysql/client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 //useNavigate = react-router-dom 설치해야한다.
 //주소이동을 위해서 사용한다.
@@ -10,6 +10,7 @@ function App() {
   const [passwd, setPasswd] = useState("");
   const [username, setUsername] = useState("");
   const [msg, setMsg] = useState("");
+  const [users, setUsers] = useState([]); //전체회원을 담기 위한 객체
 
   //const navigate = useNavigate();
 
@@ -27,6 +28,18 @@ function App() {
     );
     setMsg(data.msg);
   }
+
+  async function getUsers() {
+    const { data } = await axios.get("http://localhost:5000/api/users");
+    setUsers(data);
+  }
+
+  //화면 켜자마자 떠야 하는 데이터
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  //console.log(users);
 
   return (
     <>
@@ -61,6 +74,39 @@ function App() {
           <button type="submit">Register</button>
           {/* 버튼을 누르면 그때 axios 실행 */}
         </form>
+        <div>
+          {/* <h1 style={msg == 'success!' ? {} : null }>{msg}</h1> */}
+          값을 넣었을 때
+          <h1 style={msg == "success!" ? { backgroundColor: "yellow" } : null}>
+            {msg}
+          </h1>
+          <table border="1">
+            <thead>
+              <tr>
+                <td>ID</td>
+                <td>UserID</td>
+                <td>Password</td>
+                <td>UserName</td>
+                <td>Regdate</td>
+                <td>update/delete</td>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{user.id}</td>
+                    <td>{user.userid}</td>
+                    <td>{user.passwd}</td>
+                    <td>{user.username}</td>
+                    <td>{user.regdate}</td>
+                    <td>update/delete</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
